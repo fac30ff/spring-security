70de62b6bdb8153bdde687997f57352af2693a96
alexr <alexey.rykhalskiy@gmail.com>
2018-07-24 14:56:23 +0300

    recovery in process

---
+++ src/main/java/security/jpa/User.java
@@ -0,0 +1,59 @@
+package security.jpa;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String login;
+
+    private String passwd;
+
+    protected User() { }
+
+    public User(String login, String passwd) {
+        this.login = login;
+        this.passwd = passwd;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", login='" + login + '\'' +
+                ", passwd='" + passwd + '\'' +
+                '}';
+    }
+}
