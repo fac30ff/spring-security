ebc3422086ff69b622c33db88cd4cbfbba2d0346
alexr <alexey.rykhalskiy@gmail.com>
2018-07-27 12:52:26 +0300

    oauth2

--- src/main/java/sample/LoginController.java
+++ src/main/java/sample/LoginController.java
@@ -1,4 +1,4 @@
-package sample.rest;
+package sample;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ResolvableType;
--- src/main/java/sample/WebSecurityConfig.java
+++ src/main/java/sample/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package sample.security;
+package sample;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
