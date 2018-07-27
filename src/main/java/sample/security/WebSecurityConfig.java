8836fc8c3b1883fcb4aa9f22120a73347ed2f666
alexr <alexey.rykhalskiy@gmail.com>
2018-07-27 11:52:43 +0300

    ideas

--- src/main/java/sample/security/WebSecurityConfig.java
+++ src/main/java/sample/security/WebSecurityConfig.java
@@ -1,27 +1,14 @@
 package sample.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
-import org.springframework.security.web.authentication.logout.LogoutHandler;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
-import org.springframework.security.web.util.matcher.RegexRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.Arrays;
 
 @EnableWebSecurity
@@ -48,8 +35,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                             }});
                 }}
         );
-
-
 
         http.authorizeRequests()
                 .antMatchers("/u/**").permitAll()
